0ed07884c7884284740f3b8e9497dc26ecba83f7
BL <1078425704@qq.com>
2017-12-07 23:37:34 +0800

    55555

---
+++ resources/views/layouts/admin_menu.blade.php
@@ -0,0 +1,74 @@
+<li class="navigation" id="navigation">
+  <a href="#" class="sidebar-toggle" data-toggle="#navigation">数据管理<i class="fa fa-angle-up"></i></a>
+  <ul class="menu">
+    
+    <!-- 用户管理 -->
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i>用户管理<b class="fa fa-plus dropdown-plus"></b></a>
+      <ul class="dropdown-menu">
+<<<<<<< HEAD
+          <li>
+            <a href="form-elements.html">
+              <i class="fa fa-caret-right"></i>
+              会员列表
+            </a>
+          </li>
+          <li>
+            <a href="validation-elements.html">
+              <i class="fa fa-caret-right"></i>
+              添加会员
+            </a>
+          </li>
+          <li>
+            <a href="form-wizard.html">
+              <i class="fa fa-caret-right"></i>
+              待激活会员
+            </a>
+          </li>
+      </ul>
+    </li>
+
+    <!-- 后台管理员管理 -->
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i>管理员管理<b class="fa fa-plus dropdown-plus"></b></a>
+      <ul class="dropdown-menu">
+          <li>
+            <a href="#">
+              <i class="fa fa-caret-right"></i>
+              管理员列表
+            </a>
+          </li>
+          <li>
+            <a href="/admin/manager/create">
+              <i class="fa fa-caret-right"></i>
+              添加管理员
+            </a>
+          </li>          
+=======
+        <li>
+          <a href="form-elements.html">
+            <i class="fa fa-caret-right"></i>
+            会员列表
+          </a>
+        </li>
+        <li>
+          <a href="/admin/user/create">
+            <i class="fa fa-caret-right"></i>
+            添加会员
+          </a>
+        </li>
+        <li>
+          <a href="form-wizard.html">
+            <i class="fa fa-caret-right"></i>
+            待激活会员
+          </a>
+        </li>
+>>>>>>> e540435a08b49235c09760f413f6ea66c203caa9
+      </ul>
+    </li>
+
+    
+
+  </ul>
+
+</li>
