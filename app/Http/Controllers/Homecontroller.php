becc81d9972596d3da1680a04a2a40445231a4d1
Your Name <you@example.com>
2017-12-12 16:37:29 +0800

    个人中心

--- app/Http/Controllers/Homecontroller.php
+++ app/Http/Controllers/Homecontroller.php
@@ -8,29 +8,41 @@ use DB;
 class Homecontroller extends Controller
 {
     //个人中心
-    public function index(){
-    	$data=DB::table('class')->where('pid','0')->get();
-        foreach ($data as $k => &$v) {
-           $v->two =DB::table('class')->where('pid',$v->id)->get();
-            foreach ($v->two as $ka => &$va) {
-                $va->there =DB::table('class')->where('pid',$va->id)->get();
-            }
-        }
+    public function index(){    	
+        $sion = session('user_name');
+        $sioninfo=DB::table('user')->where('name',$sion)->select('id','name','email','phone','ztid')->first();
+        $sioninfo->phone = substr_replace($sioninfo->phone, '****', 3,4);
+        $info=DB::table('userinfo')->where('id',$sioninfo->id)->first();
+        //dd($sioninfo);
          // 分类结束
-        return view('home.index',['data'=>$data]);
+        return view('home.index',['sioninfo'=>$sioninfo,'info'=>$info]);
+    }
+
+    //编辑个人中心数据
+    public function bianji(Request $request){
+        $data=$request->only('phone','email','id');
+        $datas=[];
+        if($request->hasFile('touimg')) {
+            //获取文件的后缀名
+            $suffix = $request->file('touimg')->extension();
+            //创建一个新的随机名称
+            $name = uniqid('img_').'.'.$suffix;
+            //文件夹路径
+            $dir = './uploads/'.date('Y-m-d');
+            //移动文件
+            $request->file('touimg')->move($dir, $name);
+            //获取文件的路径
+            $datas['touimg'] = trim($dir.'/'.$name,'.');
+        }
+        $datas['shengri']=$request->input('shengri');
+
+      $xg=DB::table('user')->where('id',$request->input('id'))->update($data);
+      $touimg=DB::table('userinfo')->where('id',$request->input('id'))->update($datas);
+      return redirect('/home');
     }
 
      //订单
     public function order(){
-        // 分类开始
-        $data=DB::table('class')->where('pid','0')->get();
-        foreach ($data as $k => &$v) {
-           $v->two =DB::table('class')->where('pid',$v->id)->get();
-            foreach ($v->two as $ka => &$va) {
-                $va->there =DB::table('class')->where('pid',$va->id)->get();
-            }
-        }
-         // 分类结束
-        return view('home.order',['data'=>$data]);
+        return view('home.order');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,8 @@ Route::group(['middleware'=>'Login'],function(){
 
 	//个人中心
 	Route::get('/home','HomeController@index');
+	//修改个人中心的数据提交到数据库
+	Route::post('/home/bianji','HomeController@bianji');
 	//订单管理
 	Route::get('/home/order','HomeController@order');
 
